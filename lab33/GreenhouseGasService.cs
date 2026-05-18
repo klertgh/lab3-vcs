86a30ec834194b89b2b35b544edc2529a81a3aae
klertgh <kira2006let@mail.ru>
2026-05-18 18:35:02 +0300

    Добавлены классы связанные с парниковыми газами

---
+++ lab33/GreenhouseGasService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab33
+{
+    public class GreenhouseGasService
+    {
+        public List<GreenhouseGasRecord> LoadFromCsv(string filePath)
+        {
+            return new List<GreenhouseGasRecord>();
+        }
+
+        public string AnalyzeDecrease(List<GreenhouseGasRecord> records)
+        {
+            return "";
+        }
+    }
+}
---
+++ lab33/MovingAverageForecaster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab33
+{
+    public class MovingAverageForecaster
+    {
+        public List<double> Forecast(
+            List<double> values,
+            int movingAverageWindow,
+            int forecastCount)
+        {
+            return new List<double>();
+        }
+    }
+}
